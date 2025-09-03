2e89dbf529d45982e25b0ed99f46577823b864d2
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2025-09-03 10:51:57 -0400

    week 2

---
+++ LabWeek2.py
@@ -0,0 +1,14 @@
+#num1 = input("Enter your firt number:")
+#num2 = input("Enter your second number:")
+
+#product = int(num1) * int(num2)
+
+#print (product)
+
+
+num3 = input("Enter your firt number:")
+num4 = input("Enter your second number:")
+
+qoutent = int(num3) / int(num4)
+
+print (qoutent)
