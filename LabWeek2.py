4ea95302d1dd4078b5fc923408c00f0158e0c744
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2025-09-04 10:09:05 -0400

    LabWeek2Done

--- LabWeek2.py
+++ LabWeek2.py
@@ -1,21 +1,34 @@
-#num1 = input("Enter your firt number:")
-#num2 = input("Enter your second number:")
+#Multiplication 
+num1 = input("Enter your firt number:")
+num2 = input("Enter your second number:")
 
-#product = int(num1) * int(num2)
+product = int(num1) * int(num2)
 
-#print (product)
+print (product)
 
+#divison 
+num3 = input("Enter your firt number:")
+num4 = input("Enter your second number:")
 
-#num3 = input("Enter your firt number:")
-#num4 = input("Enter your second number:")
+qoutent = int(num3) / int(num4)
 
-#qoutent = int(num3) / int(num4)
-
-#print (qoutent)
-
+print (qoutent)
+# sqr
 num5 = input("Enter your firt number:")
 num6 = .5
 
 Sqr = float(num5) ** float(num6)
 
 print (Sqr)
+
+
+#Part Four Movie 
+
+print ("====================")
+print ("    Donie Darko")
+print ("====================")
+
+print ("Donie Darko ")
+print ("The Rabbit")
+print ("Gretchen Ross")
+print ("sparrow")
