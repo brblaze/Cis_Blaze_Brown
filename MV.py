518d044d2d09227099b1ee5fbc08a22b45ddb82e
SANDHILLS\lh178-07 <jad.talbert@gmail.com>
2025-09-04 11:29:24 -0400

    Done

---
+++ MV.py
@@ -0,0 +1,10 @@
+#Part Four Movie 
+
+print ("====================")
+print ("    Donie Darko")
+print ("====================")
+
+print ("Donie Darko ")
+print ("The Rabbit")
+print ("Gretchen Ross")
+print ("sparrow")
