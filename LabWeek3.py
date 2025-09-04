0eee809c75c566521fb50ceede9442204ec9642c
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2025-09-04 10:38:37 -0400

    lab3

---
+++ LabWeek3.py
@@ -0,0 +1,7 @@
+#distance
+
+velocity = 80
+time = 2
+distance = float(velocity) * float(time)
+
+print ('the distance is (velocity) mph for (time) hours is (distance) miles traveld') .format (velocity, time, distance)
