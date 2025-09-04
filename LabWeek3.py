add26843d7fff4679d0558ba6008322e04ac794c
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2025-09-04 10:48:40 -0400

    distance

--- LabWeek3.py
+++ LabWeek3.py
@@ -1,7 +1,5 @@
 #distance
-
 velocity = 80
 time = 2
-distance = float(velocity) * float(time)
-
-print ('the distance is (velocity) mph for (time) hours is (distance) miles traveld') .format (velocity, time, distance)
+distance = float(velocity)*float(time)
+print("the velocity of {0} miles per hour multiplied by {1} hours equals the distanse of {2}")
