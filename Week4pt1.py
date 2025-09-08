6cefcd363f86891bc2436a9b528e368fa686618b
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2025-09-08 10:47:28 -0400

    dome

---
+++ Week4pt1.py
@@ -0,0 +1,12 @@
+num1=input('enter first number ')
+num2= input('enter second number')
+num3=input ('enter third number')
+sum= int(num1)+int(num2)
+
+qoutent=sum/int(num3)
+
+if qoutent >0:
+     print (f'The mathematical opperation is {qoutent} >0 ') 
+
+else:
+     print (f'The mathematical operation is{qoutent}=<0')
