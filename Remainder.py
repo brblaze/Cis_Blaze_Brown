7bda351a626a82c91d01901de51cb1466a564589
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2025-09-10 10:25:33 -0400

    done

--- Remainder.py
+++ Remainder.py
@@ -1,4 +1,8 @@
 #numer 3 lab week 4
 num1=input('enter first number')
 num2=input('enter second number')
-qoutent=num1/num2
+qoutent=int(num1)%int(num2)
+if qoutent==0:
+    print ('remainder is even')
+else: 
+    print ('remainder is odd')
