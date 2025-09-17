f91a5f537e36b1c2d735b6e3c38855bf54f432e5
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2025-09-17 10:50:10 -0400

    workedon

--- Week5/enterGrades.py
+++ Week5/enterGrades.py
@@ -1,1 +1,9 @@
-numberofgrades= int(input('How many grades do you want to grade' ))
+numberofgrades= int(input('How many grades do you want to grade ' ))
+
+n=0
+print (f"{numberofgrades}")
+#while loop
+while n < numberofgrades:
+ n=n+1
+ grades=100
+ print (f'{grades}')
